54de716dcdf5d5d4de0a037826c1809ee8e5ec4b
Tianzi Cai <tianzi@google.com>
2022-07-26 16:52:46 -0700

    structure it with GCE startup script and metadata

--- src/test/java/it/Base.java
+++ src/test/java/it/Base.java
@@ -2,18 +2,38 @@ package it;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.google.cloud.compute.v1.AccessConfig;
+import com.google.cloud.compute.v1.AccessConfig.NetworkTier;
+import com.google.cloud.compute.v1.AttachedDisk;
+import com.google.cloud.compute.v1.AttachedDiskInitializeParams;
+import com.google.cloud.compute.v1.InsertInstanceRequest;
+import com.google.cloud.compute.v1.InsertInstanceTemplateRequest;
+import com.google.cloud.compute.v1.Instance;
+import com.google.cloud.compute.v1.InstanceProperties;
+import com.google.cloud.compute.v1.InstanceTemplate;
+import com.google.cloud.compute.v1.InstanceTemplatesClient;
+import com.google.cloud.compute.v1.InstancesClient;
+import com.google.cloud.compute.v1.Items;
+import com.google.cloud.compute.v1.Metadata;
+import com.google.cloud.compute.v1.NetworkInterface;
+import com.google.cloud.compute.v1.Operation;
+import com.google.cloud.compute.v1.ServiceAccount;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.common.flogger.GoogleLogger;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
 import org.apache.maven.shared.invoker.DefaultInvoker;
@@ -28,15 +48,17 @@ public class Base {
 
   private static final GoogleLogger log = GoogleLogger.forEnclosingClass();
 
-  // TODO: use `System.getenv("BUCKET_NAME");`
+  // TODO: System.getenv("BUCKET_NAME")
   private static final String bucketName = "pubsublite-it";
-  protected final String runId = UUID.randomUUID().toString();
+  protected static final String runId = UUID.randomUUID().toString().substring(0, 8);
   protected String mavenHome;
   protected String workingDir;
   protected String connectorVersion;
   protected String connectorJarName;
   protected String connectorJarNameInGCS;
   protected String connectorJarLoc;
+  protected String kafkaVersion;
+  protected String scalaVersion;
 
   protected void findMavenHome() throws Exception {
     Process p = Runtime.getRuntime().exec("mvn --version");
@@ -92,11 +114,142 @@ public class Base {
     connectorJarNameInGCS =
         String.format("pubsub-group-kafka-connector-%s-%s.jar", connectorVersion, runId);
     connectorJarLoc = String.format("%s/target/%s", workingDir, connectorJarName);
+
+    // TODO: Get Kafka and Scala versions programmatically: {major}.{minor}.{patch}.
+    kafkaVersion = "3.2.0";
+    scalaVersion = "2.13";
   }
 
   protected void uploadGCS(Storage storage, String fileNameInGCS, String fileLoc) throws Exception {
     BlobId blobId = BlobId.of(bucketName, fileNameInGCS);
     BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
     storage.create(blobInfo, Files.readAllBytes(Paths.get(fileLoc)));
+  }
+
+  protected void createInstanceTemplate(
+      String projectId, String projectNumber, String instanceTemplateName)
+      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    try (InstanceTemplatesClient instanceTemplatesClient = InstanceTemplatesClient.create()) {
+
+      String machineType = "e2-medium";
+      String sourceImage = "projects/debian-cloud/global/images/family/debian-11";
+
+      // The template describes the size and source image of the boot disk
+      // to attach to the instance.
+      AttachedDisk attachedDisk =
+          AttachedDisk.newBuilder()
+              .setInitializeParams(
+                  AttachedDiskInitializeParams.newBuilder()
+                      .setSourceImage(sourceImage)
+                      .setDiskSizeGb(10)
+                      .build())
+              .setAutoDelete(true)
+              .setBoot(true)
+              .build();
+
+      // The template connects the instance to the `default` network,
+      // without specifying a subnetwork.
+      NetworkInterface networkInterface =
+          NetworkInterface.newBuilder()
+              .setName("global/networks/default")
+              // The template lets the instance use an external IP address.
+              .addAccessConfigs(
+                  AccessConfig.newBuilder()
+                      .setName("External NAT")
+                      .setType(AccessConfig.Type.ONE_TO_ONE_NAT.toString())
+                      .setNetworkTier(NetworkTier.PREMIUM.toString())
+                      .build())
+              .build();
+
+      // TODO: flesh out the complete startup script. Possibly link to a file.
+      Metadata metadata =
+          Metadata.newBuilder()
+              .addItems(
+                  Items.newBuilder()
+                      .setKey("url")
+                      .setValue(
+                          String.format(
+                              "https://downloads.apache.org/kafka/%s/kafka_%s-%s.tgz",
+                              kafkaVersion, scalaVersion, kafkaVersion))
+                      .build())
+              .addItems(
+                  Items.newBuilder()
+                      .setKey("startup-script")
+                      .setValue(
+                          "#! /bin/bash\nURL=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/url -H \"Metadata-Flavor: Google\")\nsudo apt-get update\nsudo apt-get install -yq wget openjdk-11-jdk maven\nwget $URL")
+                      .build())
+              .build();
+
+      InstanceProperties instanceProperties =
+          InstanceProperties.newBuilder()
+              .addDisks(attachedDisk)
+              .setMachineType(machineType)
+              .addNetworkInterfaces(networkInterface)
+              .addServiceAccounts(
+                  ServiceAccount.newBuilder()
+                      .setEmail(
+                          String.format("%s-compute@developer.gserviceaccount.com", projectNumber))
+                      .addScopes("https://www.googleapis.com/auth/pubsub")
+                      .build())
+              .setMetadata(metadata)
+              .build();
+
+      InsertInstanceTemplateRequest insertInstanceTemplateRequest =
+          InsertInstanceTemplateRequest.newBuilder()
+              .setProject(projectId)
+              .setInstanceTemplateResource(
+                  InstanceTemplate.newBuilder()
+                      .setName(instanceTemplateName)
+                      .setProperties(instanceProperties)
+                      .build())
+              .build();
+
+      // Create the Instance Template.
+      Operation response =
+          instanceTemplatesClient
+              .insertAsync(insertInstanceTemplateRequest)
+              .get(3, TimeUnit.MINUTES);
+
+      TimeUnit.MINUTES.sleep(3);
+
+      if (response.hasError()) {
+        System.out.println("\nInstance Template creation failed ! ! " + response);
+        return;
+      }
+      System.out.printf(
+          "\nInstance Template Operation Status %s: %s",
+          instanceTemplateName, response.getStatus());
+    }
+  }
+
+  protected void createInstanceFromTemplate(
+      String projectId, String zone, String instanceName, String instanceTemplateName)
+      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+
+    try (InstancesClient instancesClient = InstancesClient.create()) {
+
+      InsertInstanceRequest insertInstanceRequest =
+          InsertInstanceRequest.newBuilder()
+              .setProject(projectId)
+              .setZone(zone)
+              .setInstanceResource(Instance.newBuilder().setName(instanceName).build())
+              .setSourceInstanceTemplate(
+                  String.format(
+                      "projects/%s/global/instanceTemplates/%s", projectId, instanceTemplateName))
+              .build();
+
+      Operation response =
+          instancesClient.insertAsync(insertInstanceRequest).get(3, TimeUnit.MINUTES);
+
+      TimeUnit.MINUTES.sleep(3);
+
+      if (response.hasError()) {
+        System.out.println("\nInstance creation from template failed ! ! " + response);
+        return;
+      }
+      System.out.printf(
+          "\nInstance creation from template: Operation Status %s: %s ",
+          instanceName, response.getStatus());
+    }
   }
 }
--- src/test/java/it/StandaloneIT.java
+++ src/test/java/it/StandaloneIT.java
@@ -1,10 +1,8 @@
 package it;
 
 import com.google.api.gax.rpc.NotFoundException;
-import com.google.cloud.compute.v1.InsertInstanceRequest;
-import com.google.cloud.compute.v1.Instance;
+import com.google.cloud.compute.v1.InstanceTemplatesClient;
 import com.google.cloud.compute.v1.InstancesClient;
-import com.google.cloud.compute.v1.Operation;
 import com.google.cloud.pubsub.v1.SubscriptionAdminClient;
 import com.google.cloud.pubsub.v1.TopicAdminClient;
 import com.google.cloud.storage.Storage;
@@ -14,12 +12,8 @@ import com.google.pubsub.v1.Subscription;
 import com.google.pubsub.v1.SubscriptionName;
 import com.google.pubsub.v1.TopicName;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.PrintStream;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -33,16 +27,15 @@ public class StandaloneIT extends Base {
   private static final GoogleLogger log = GoogleLogger.forEnclosingClass();
 
   private static final String projectId = System.getenv("GOOGLE_CLOUD_PROJECT");
-  private static final String _suffix = UUID.randomUUID().toString().substring(0, 6);
-  private static final String sourceTopicId = "source-topic-" + _suffix;
-  private static final String sourceSubscriptionId = "source-subscription-" + _suffix;
-  private static final String sinkTopicId = "sink-topic-" + _suffix;
-  private static final String sinkSubscriptionId = "sink-subscription-" + _suffix;
+  // TODO: System.getenv("GOOGLE_CLOUD_PROJECT_NUMBER");
+  private static final String projectNumber = "779844219229";
+  private static final String sourceTopicId = "source-topic-" + runId;
+  private static final String sourceSubscriptionId = "source-subscription-" + runId;
+  private static final String sinkTopicId = "sink-topic-" + runId;
+  private static final String sinkSubscriptionId = "sink-subscription-" + runId;
   private static final String zone = "us-central1-b";
-  private static final String instanceName = "kafka-it-" + _suffix;
-  private static final String instanceTemplateName =
-      String.format(
-          "projects/%s/global/instanceTemplates/%s", projectId, "pubsub-kafka-it-template");
+  private static final String instanceName = "kafka-it-" + runId;
+  private static final String instanceTemplateName = "kafka-it-template-" + runId;
   private Boolean initialized = false;
 
   private static final TopicName sourceTopicName = TopicName.of(projectId, sourceTopicId);
@@ -64,14 +57,15 @@ public class StandaloneIT extends Base {
       return;
     }
 
-    log.atInfo().log("RunId is: %s", _suffix);
     findMavenHome();
     setupVersions();
     mavenPackage(workingDir);
     log.atInfo().log("Packaged connector jar.");
+
     Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
     uploadGCS(storage, connectorJarNameInGCS, connectorJarLoc);
     log.atInfo().log("Uploaded connector jar to GCS.");
+
     initialized = true;
 
     try (TopicAdminClient topicAdminClient = TopicAdminClient.create()) {
@@ -93,6 +87,12 @@ public class StandaloneIT extends Base {
               .build());
       log.atInfo().log("Crated source and sink subscriptions");
     }
+
+    createInstanceTemplate(projectId, projectNumber, instanceTemplateName);
+    log.atInfo().log("Created Compute Engine instance template.");
+
+    createInstanceFromTemplate(projectId, zone, instanceName, instanceTemplateName);
+    log.atInfo().log("Created Compute Engine instance from instance template");
   }
 
   @After
@@ -101,9 +101,9 @@ public class StandaloneIT extends Base {
       try {
         subscriptionAdminClient.deleteSubscription(sourceSubscriptionName);
         subscriptionAdminClient.deleteSubscription(sinkSubscriptionName);
-        log.atInfo().log("Deleted source and sink subscriptions");
+        log.atInfo().log("Deleted source and sink subscriptions.");
       } catch (NotFoundException ignored) {
-        // Ignore this as resources may not have been created.
+        log.atInfo().log("Ignore. No topics found.");
       }
     }
 
@@ -112,49 +112,26 @@ public class StandaloneIT extends Base {
       topicAdminClient.deleteTopic(sinkTopicName.toString());
       log.atInfo().log("Deleted source and sink topics");
     } catch (NotFoundException ignored) {
-      // Ignore this as resources may not have been created.
+      log.atInfo().log("Ignore. No subscriptions found.");
     }
+
+    try (InstancesClient instancesClient = InstancesClient.create()) {
+      instancesClient.deleteAsync(projectId, zone, instanceName).get(3, TimeUnit.MINUTES);
+    }
+    log.atInfo().log("Deleted Compute Engine instance.");
+
+    try (InstanceTemplatesClient instanceTemplatesClient = InstanceTemplatesClient.create()) {
+      instanceTemplatesClient.deleteAsync(projectId, instanceTemplateName).get(3, TimeUnit.MINUTES);
+    }
+    log.atInfo().log("Deleted Compute Engine instance template.");
+
     System.setOut(null);
   }
 
   @Test
   public void endToEndTest() throws Exception {
-    createInstanceFromTemplate(projectId, zone, instanceName, instanceTemplateName);
-
-    // TODO:
-    // Copy connector jar from GCS to instance
-    // Update connect and connector properties on instance, both source and sink
-    // SSH into instance and start up Zookeeper
-    // SSH into instance and start up Kafka
-    // SSH into instance and create a topic
+    // TODO: Implement the rest.
     // Publish messages to source topic
     // Assert messages can be pulled from sink subscription
-  }
-
-  public static void createInstanceFromTemplate(
-      String projectId, String zone, String instanceName, String instanceTemplateName)
-      throws IOException, ExecutionException, InterruptedException, TimeoutException {
-
-    try (InstancesClient instancesClient = InstancesClient.create()) {
-
-      InsertInstanceRequest insertInstanceRequest =
-          InsertInstanceRequest.newBuilder()
-              .setProject(projectId)
-              .setZone(zone)
-              .setInstanceResource(Instance.newBuilder().setName(instanceName).build())
-              .setSourceInstanceTemplate(instanceTemplateName)
-              .build();
-
-      Operation response =
-          instancesClient.insertAsync(insertInstanceRequest).get(3, TimeUnit.MINUTES);
-
-      if (response.hasError()) {
-        System.out.println("Instance creation from template failed ! ! " + response);
-        return;
-      }
-      System.out.printf(
-          "Instance creation from template: Operation Status %s: %s ",
-          instanceName, response.getStatus());
-    }
   }
 }
